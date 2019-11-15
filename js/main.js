8aca834a66f5c12327391d196a998da6951fcfc4
Roger Martinez <rgmartinez@google.com>
2019-11-15 15:05:56 -0800

    showAsTime function

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-// const PomSesh = function(length=25){
-//     this.timeStart = new Date(),
-//     this.id = this.timeStart.getTime(),
-//     this.length = length*60000,
-//     this.status = 'started',
-//     this.timeEnd = new Date(this.timeStart.getTime() + length*60000),
-//     this.timeLeft = function() {
-//         const now = new Date()
-//         if (now > this.timeEnd){
-//             this.status = 'finished';
-//             return 0
-//         }
-//         const milliseconds = this.timeEnd - now;
-//         const minutes = Math.floor(milliseconds / 60000);
-//         const seconds = parseInt(((milliseconds % 60000)/1000).toFixed(0))
-//         return [minutes, seconds]
-//     },
-//     this.reset = function() {
-//         const now = new Date()
-//         this.timeEnd = new Date(now.getTime() + length*60000);
-//     }
-// }
-
 class PomSesh {
     constructor(length=25){
         this.timeStart = new Date(),
@@ -40,7 +17,7 @@ class PomSesh {
         return [minutes, seconds]
     }
     reset() {
-        const now = new Date()
+        const now = new Date();
         this.timeEnd = new Date(now.getTime() + length*60000)
     }
         // this.reset = function() {
@@ -55,8 +32,18 @@ function newTimer(length){
     return current;
 }
 
-let session = newTimer(25);
-
-console.log(session)
-console.log(session.timeLeft())
-
+function showAsTime(minutes, seconds) {
+    if (seconds === 60) {
+        minutes = minutes+1;
+        seconds = 0;
+    }
+    let time = [minutes, seconds]
+    time = time.map(x=>{
+        if (x < 10){
+            return '0'+x.toString()}
+        else {return x.toString()}
+    } )
+    return time.join(':')
+}
+console.log(showAsTime(0, 22))
+console.log(showAsTime(2, 4))
