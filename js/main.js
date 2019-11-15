fc8960a2a25b32e8e30ea529f976315cb24be300
Roger Martinez <rgmartinez@google.com>
2019-11-15 12:53:14 -0800

    change to class declaration

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-const PomSesh = function(length=25){
-    this.timeStart = new Date(),
-    this.id = this.timeStart.getTime(),
-    this.length = length*60000,
-    this.status = 'started',
-    this.timeEnd = new Date(this.timeStart.getTime() + length*60000),
-    this.timeLeft = function() {
+// const PomSesh = function(length=25){
+//     this.timeStart = new Date(),
+//     this.id = this.timeStart.getTime(),
+//     this.length = length*60000,
+//     this.status = 'started',
+//     this.timeEnd = new Date(this.timeStart.getTime() + length*60000),
+//     this.timeLeft = function() {
+//         const now = new Date()
+//         if (now > this.timeEnd){
+//             this.status = 'finished';
+//             return 0
+//         }
+//         const milliseconds = this.timeEnd - now;
+//         const minutes = Math.floor(milliseconds / 60000);
+//         const seconds = parseInt(((milliseconds % 60000)/1000).toFixed(0))
+//         return [minutes, seconds]
+//     },
+//     this.reset = function() {
+//         const now = new Date()
+//         this.timeEnd = new Date(now.getTime() + length*60000);
+//     }
+// }
+
+class PomSesh {
+    constructor(length=25){
+        this.timeStart = new Date(),
+        this.timeEnd = new Date(this.timeStart.getTime() + length*60000),
+        this.id = this.timeStart.getTime(),
+        this.length = length*60000,
+        this.status = 'started'}
+    timeLeft() {
         const now = new Date()
         if (now > this.timeEnd){
             this.status = 'finished';
@@ -14,14 +38,25 @@ const PomSesh = function(length=25){
         const minutes = Math.floor(milliseconds / 60000);
         const seconds = parseInt(((milliseconds % 60000)/1000).toFixed(0))
         return [minutes, seconds]
-    },
-    this.reset = function() {
-        const now = new Date()
-        this.timeEnd = new Date(now.getTime() + length*60000);
     }
+    reset() {
+        const now = new Date()
+        this.timeEnd = new Date(now.getTime() + length*60000)
+    }
+        // this.reset = function() {
+        //     const now = new Date()
+        //     this.timeEnd = new Date(now.getTime() + length*60000);
+        // }
 }
+
 
 function newTimer(length){
     current = new PomSesh(length);
     return current;
 }
+
+let session = newTimer(25);
+
+console.log(session)
+console.log(session.timeLeft())
+
